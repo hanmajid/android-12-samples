680ec7b51963d68c7c058dde42daa1e7d41e0ea1
hanmajid <han.majid2004@gmail.com>
2022-03-17 09:06:43 +0700

    Init audio-coupled-haptic-effects project

---
+++ audio-coupled-haptic-effects/app/src/main/java/com/hanmajid/android/snowcone/audiocoupledhapticeffects/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hanmajid.android.snowcone.audiocoupledhapticeffects
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
