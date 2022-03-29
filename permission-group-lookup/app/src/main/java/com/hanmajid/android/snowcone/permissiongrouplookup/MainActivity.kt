416a6b7adf9bfdb0b4cc9526df6e3cb0d87234e7
hanmajid <han.majid2004@gmail.com>
2022-03-29 10:43:42 +0700

    Finish permission-group-lookup project

--- permission-group-lookup/app/src/main/java/com/hanmajid/android/snowcone/permissiongrouplookup/MainActivity.kt
+++ permission-group-lookup/app/src/main/java/com/hanmajid/android/snowcone/permissiongrouplookup/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.hanmajid.android.snowcone.permissiongrouplookup
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+    private val executor = Executors.newSingleThreadExecutor()
+
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Gets the permission group of `ACCESS_FINE_LOCATION` permission.
+        packageManager.getGroupOfPlatformPermission(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            executor,
+        ) {
+            runOnUiThread {
+                findViewById<TextView>(R.id.permission_group).text = it
+            }
+        }
+        // Gets the permissions of `STORAGE` permission group.
+        packageManager.getPlatformPermissionsForGroup(
+            Manifest.permission_group.STORAGE,
+            executor,
+        ) {
+            runOnUiThread {
+                findViewById<TextView>(R.id.permissions).text = it.joinToString(", ")
+            }
+        }
     }
 }
